a20de34030cbb2d03d86471be64102f564308d5a
Marioly V <mariolyv7@gmail.com>
2020-12-01 17:27:02 -0400

    Agregando inicio de sesion por medio de email y password con conexion a BBDD

--- Frontend/app/src/main/java/com/example/frontend/activity/LogInActivity.java
+++ Frontend/app/src/main/java/com/example/frontend/activity/LogInActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.frontend.R;
 import com.example.frontend.api.ChatApi;
@@ -20,9 +22,11 @@ import java.util.ArrayList;
 
 public class LogInActivity extends AppCompatActivity {
 
+    private EditText etName;
+    private EditText etPassword;
     private Button loginButton;
     private Button signupButton;
-    int userId = 1;
+    int userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,44 +35,50 @@ public class LogInActivity extends AppCompatActivity {
 
         loginButton = findViewById(R.id.login_button);
         signupButton = findViewById(R.id.signup_button);
+        etName = findViewById(R.id.email_edit);
+        etPassword = findViewById(R.id.password_edit);
 
-       /* HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.level(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient httpClient = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
-
-        Retrofit retrofit=new Retrofit.Builder()
-                //.baseUrl("https://jsonplaceholder.typicode.com/")
-                .baseUrl("http://192.168.31.148:8081/v1/user/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient)
-                .build();
-        UserApi userApi= retrofit.create(UserApi.class);
-
-        Call<Integer> call = userApi.getId();
-
-        call.enqueue(new Callback<Integer>() {
-            @Override
-            public void onResponse(Call<Integer> call, Response<Integer> response) {
-                if (!response.isSuccessful()) {
-                    //textViewResult.setText("Code: " + response.code());
-                    return;
-                }
-                userId = response.body();
-            }
-
-            @Override
-            public void onFailure(Call<Integer> call, Throwable t) {
-                //textViewResult.setText(t.getMessage());
-                return;
-            }
-        });
-*/
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (LogInActivity.this, MainActivity.class);
-                intent.putExtra("userId",userId);
-                startActivity(intent);
+                if(etPassword.getText().toString().isEmpty()||etName.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Debe llenar todos los campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+                    loggingInterceptor.level(HttpLoggingInterceptor.Level.BODY);
+                    OkHttpClient httpClient = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+
+                    Retrofit retrofit=new Retrofit.Builder()
+                            //.baseUrl("https://jsonplaceholder.typicode.com/")
+                            .baseUrl("http://192.168.31.148:8081/v1/user/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .client(httpClient)
+                            .build();
+                    UserApi userApi= retrofit.create(UserApi.class);
+
+                    Call<Integer> call = userApi.getId(etName.getText().toString(),etPassword.getText().toString());
+
+                    call.enqueue(new Callback<Integer>() {
+                        @Override
+                        public void onResponse(Call<Integer> call, Response<Integer> response) {
+                            if (!response.isSuccessful()) {
+                                etName.setText("Code: " + response.code());
+                                return;
+                            }
+                            userId = response.body();
+                            Intent intent = new Intent (LogInActivity.this, MainActivity.class);
+                            intent.putExtra("userId",userId);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onFailure(Call<Integer> call, Throwable t) {
+                            etName.setText(t.getMessage());
+                            return;
+                        }
+                    });
+                }
             }
         });
 
--- Frontend/app/src/main/java/com/example/frontend/api/UserApi.java
+++ Frontend/app/src/main/java/com/example/frontend/api/UserApi.java
@@ -3,17 +3,14 @@ package com.example.frontend.api;
 import com.example.frontend.model.User;
 import com.example.frontend.model.UserRequest;
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 import java.util.ArrayList;
 
 public interface UserApi {
 
-    @GET("/userRequest/login?email=jps@gmail.com&password=vdsvds44")
-    Call<Integer> getId();
+    @GET("userRequest/login")
+    Call<Integer> getId(@Query("email") String email, @Query("password") String password);
 
     /*
     @FormUrlEncoded
